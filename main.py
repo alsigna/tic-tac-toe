9f1a2ab4bd8b8d63c1cc0ff1901e9c315c68aca0
Alexander Ignatov <ignatov.alx@gmail.com>
2021-04-09 09:30:47 +0300

    add docstring, catch exceptions,

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-def draw_field(data):
-    print(" " * 4 + "   ".join([str(i) for i in range(size)]))
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def draw_field(play_progress):
+    """Draw table with 'X' and 'O'
+        0   1   2
+      +---+---+---+
+    0 | X |   |   |
+      +---+---+---+
+    1 |   | O | O |
+      +---+---+---+
+    2 |   |   |   |
+      +---+---+---+
+
+    Args:
+        data (list of list): matrix with 'X' and 'O' positions
+    """
+    print(" " * 4 + "   ".join([str(indx) for indx in range(size)]))
     print(" ", "+---" * size + "+")
-    for i, line in enumerate(data):
-        print(str(i) + " | " + " | ".join(line) + " |")
+    for indx, line in enumerate(play_progress):
+        print(str(indx) + " | " + " | ".join(line) + " |")
         print("  " + "+---" * size + "+")
 
 
 def draw_welcome():
-    s = """
+    """Draw Welcome info."""
+    welcome_string = """
     \tДобро пожаловать.
     \tКоординаты вводятся через пробел в формате "X Y"
     """
-    print(s)
+    print(welcome_string)
 
 
 def coordinates():
+    """Getting desired cell from player.
+
+    Returns:
+        x, y (int, int): X and Y coordinates
+    """
     while True:
-        x, y = input("\tВведите координаты X Y: ").split()
+        try:
+            x, y = input("\tВведите координаты X Y: ").split()
+        except ValueError:
+            print("\t\tОшибка, нужно два числа.")
+            continue
 
         if not (x.isdigit()) or not (y.isdigit()):
             print("\t\tОшибка, введены не числа.")
@@ -35,7 +62,16 @@ def coordinates():
         return x, y
 
 
-def check_for_win():
+def check_for_winner(signs):
+    """Who is th winner.
+
+    Args:
+        signs (list): List with plaeyr's signs, ['X','O'] usualy
+
+    Returns:
+        element of signs list or None: None if no winner yet, or player's sign
+    """
+
     # rows
     for_check = field.copy()
 
@@ -43,20 +79,20 @@ def check_for_win():
     field_flat = []
     for col in field:
         field_flat.extend(col)
-    for i in range(size):
-        col = [x for indx, x in enumerate(field_flat) if indx % size == i]
+    for col_number in range(size):
+        col = [x for indx, x in enumerate(field_flat) if indx % size == col_number]
         for_check.append(col)
 
     # diags
     diag1 = []
     diag2 = []
-    for i, line in enumerate(field):
-        diag1.append(line[i])
-        diag2.append(line[-1 - i])
+    for row_number, line in enumerate(field):
+        diag1.append(line[row_number])
+        diag2.append(line[-1 - row_number])
     for_check.append(diag1)
     for_check.append(diag2)
 
-    # check for winners
+    # check for winner
     for line in for_check:
         if len(list(set(line))) == 1 and line[0] in signs:
             return line[0]
@@ -64,28 +100,54 @@ def check_for_win():
         return None
 
 
-size = int(input("Введите размер стороны поля:"))
-field = [[" " for _ in range(size)] for _ in range(size)]
-signs = ["X", "O"]
-turn = 0
+def field_initialize():
+    """Geetting field size from customer
 
-draw_welcome()
-while True:
-    turn += 1
-    draw_field(field)
-    sign = signs[turn % 2]
-    print(f"\tХодит '{sign}'")
+    Returns:
+        size (int): field size
+    """
+    while True:
+        try:
+            size = input("Введите размер стороны поля (от 3 до 9):")
+        except ValueError:
+            print("\t\tОшибка, нужно одно число")
+            continue
 
-    if turn > size ** 2:
+        if not size.isdigit():
+            print("\t\tОшибка, введено не числа.")
+            continue
+
+        size = int(size)
+
+        if not 3 <= size <= 9:
+            print("\t\tОшибка, размеры должны быть от 3 до 9")
+            continue
+
+        return size
+
+
+if __name__ == "__main__":
+    size = field_initialize()
+    draw_welcome()
+    field = [[" " for _ in range(size)] for _ in range(size)]
+    signs = ["X", "O"]
+    turn = 0
+    while True:
+        turn += 1
         draw_field(field)
-        print("\tНичья")
-        break
+        sign = signs[turn % 2]
+        print(f"\tХодит '{sign}'")
 
-    x, y = coordinates()
+        if turn > size ** 2:
+            draw_field(field)
+            print("\tНичья")
+            break
 
-    field[x][y] = sign
-    win = check_for_win()
-    if win is not None:
-        draw_field(field)
-        print(f"\n\tПобедил '{win}'")
-        break
+        x, y = coordinates()
+
+        field[x][y] = sign
+        winner = check_for_winner(signs)
+        if winner is not None:
+            draw_field(field)
+            print(f"\n\tПобедил '{winner}'")
+            break
